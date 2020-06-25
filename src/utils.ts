783d36feebd1875f25153ef1d8efb6abfe2848fd
devskope <ope.oguntoye@protonmail.com>
2020-06-25 11:17:19 +0100

    [#8] build recursive query string parts

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,15 @@ export const gtoken = new GoogleToken({
   keyFile: process.env.FIRE_SOURCE_CREDS,
   scope: ['https://www.googleapis.com/auth/datastore'],
 });
+
+export const buildRecursiveQueryString = (
+  path: string,
+  queryKey: string,
+  valueArray: string[]
+): string => {
+  if (!path.includes('?')) path += '?';
+
+  valueArray.forEach((value) => (path += `${queryKey}=${value}&`));
+
+  return path.endsWith('&') ? path.slice(0, -1) : path;
+};
